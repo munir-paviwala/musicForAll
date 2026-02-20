9c95767cd67bfe767d522c0901fcdcedf172ceb9
Munirmahedi Paviwala <munir1612@gmail.com>
2026-02-20 15:30:24 -0500

    Merge origin/main and preserve local user_config.js

--- user_config.js
+++ user_config.js
@@ -5,15 +5,15 @@
 
 const CONFIG = {
     // The name of your galaxy that appears in the browser tab and intro screen
-    pageTitle: "Munir's Music Galaxy",
+    pageTitle: "My Music Galaxy",
 
     // Your name or curator title
-    curatorName: "Munir",
+    curatorName: "Music Lover",
 
     // The introductory message spoken by the character when the page loads
-    introText: "Hello there! Welcome to my MUSICAL GALAXY!\n\nI am ✨Munir✨\n\nTake your time exploring the stars and listening to the music.\n\nThen, if you want to create a universe of your own, look for the '✨ Make Your Own Galaxy' button below!",
+    introText: "Hello there! Welcome to my MUSICAL GALAXY!\n\nI am your guide in this neighborhood.\n\nTake your time exploring the stars and listening to the music.\n\nThen, if you want to create a universe of your own, look for the '✨ Make Your Own Galaxy' button below!",
 
     // The seed used to generate the galaxy layout uniquely
     // (Changing this will completely reshuffle the spatial placement of clusters)
-    galaxySeed: "LetsHaveFun"
+    galaxySeed: "MyMusicGalaxySeed"
 };
