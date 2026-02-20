cbbb3fb4752b828b8a2c11454181be43273960d9
Munirmahedi Paviwala <munir1612@gmail.com>
2026-02-20 10:12:30 -0500

    retro setup page

--- user_config.js
+++ user_config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     curatorName: "Music Lover",
 
     // The introductory message spoken by the character when the page loads
-    introText: "Hello there! Welcome to my MUSICAL GALAXY!\n\nI am your guide in this neighborhood.\n\nIf you want to create a musical universe of your own, look for the 'Make Your Own Galaxy' button below!",
+    introText: "Hello there! Welcome to my MUSICAL GALAXY!\n\nI am your guide in this neighborhood.\n\nTake your time exploring the stars and listening to the music.\n\nThen, if you want to create a universe of your own, look for the '✨ Make Your Own Galaxy' button below!",
 
     // The seed used to generate the galaxy layout uniquely
     // (Changing this will completely reshuffle the spatial placement of clusters)
